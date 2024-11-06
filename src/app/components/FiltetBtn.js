1f0b38dd9271d13c9774e94eb2429f2b53050183
Pouya <pouyasamadnejad@gmail.com>
2024-11-07 01:41:02 +0330

    some codes changed

--- src/app/components/FiltetBtn.js
+++ src/app/components/FiltetBtn.js
@@ -4,7 +4,7 @@ import { FiFilter } from "react-icons/fi";
 function FiltetBtn() {
   return (
     <div className="lg:hidden w-full flex justify-center ">
-      <button className="text-white bg-[#0F0F10] py-8 flex items-center justify-center w-4/5 font-bold gap-2 rounded-full fixed z-50 bottom-2 ">
+      <button className="text-white bg-[#0F0F10] py-4 flex items-center justify-center w-4/5 font-bold gap-2 rounded-full fixed z-50 bottom-2 ">
         <FiFilter />
         اعمال فیلتر آگهی
       </button>
